79cd2ffdeb2f88a2bdd3fd6bd5ced29125a2e524
BURNERLIGHT <55771512+BURNERLIGHT@users.noreply.github.com>
2020-11-04 20:03:57 +0800

    small_2

---
+++ app/src/main/java/com/example/hid_intent01/Another_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hid_intent01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Another_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_);
+    }
+}
